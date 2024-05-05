674af7bfa5580a4042bb7b6de77437766d3b6cd0
=raj-maharjan99 <=rjuallone@gmail.com>
2024-05-05 10:04:03 -0500

    create login api with jsonwebtoken with fully funcationable

--- controller/userLoginController.js
+++ controller/userLoginController.js
@@ -1,7 +1,53 @@
 import { User } from "../model/User.js";
 import bcrypt from "bcrypt";
-export const userLogin = (req, res) => {
-  console.log(req.body);
+import jwt from "jsonwebtoken";
+export const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existUser = await User.findOne({
+      email: email,
+    });
+    console.log(existUser);
+    if (existUser) {
+      const pass = bcrypt.compareSync(password, existUser.password);
+      if (!pass) {
+        return res.status(401).json({
+          status: "error",
+          message: "username or password doesn't match",
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          id: existUser._id,
+          isAdmin: existUser.isAdmin,
+        },
+        "jwtsecret"
+      );
+
+      return res.status(201).json({
+        status: "successfully Logged In.",
+        data: {
+          token,
+          id: existUser._id,
+          isAdmin: existUser.isAdmin,
+          email: existUser.email,
+          fullname: existUser.fullname,
+          // shippingAddresss: existUser.shippingAddresss,
+        },
+      });
+    } else {
+      return res.status(401).json({
+        status: "error",
+        message: "user doesn't  exist",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: `${error}`,
+    });
+  }
 
   res.status(200).send("userlogin api from controller");
 };
